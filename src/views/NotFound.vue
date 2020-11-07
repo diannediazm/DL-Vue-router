fcacb8753818d8cab83f7289754baefe1cba3b25
Dianne <dianne.diazm@gmail.com>
2020-11-07 18:37:32 -0300

    quinto commit

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,16 +1,18 @@
 <!-- CONTENIDO PARA LA VISTA DE RUTAS INEXISTENTES -->
-<header class="masthead" style="background-image: url('/assets/img/chevrolet-blazer.jpg');">
-    <div class="overlay"></div>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-10 col-lg-8 mx-auto">
-                <div class="post-heading">
-                    <h1><strong>La página que busca no existe</strong></h1>
+<template>
+    <header class="masthead-not" style="background-image: url('/assets/img/chevrolet-blazer.jpg');">
+        <div class="overlay"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-10 col-lg-8 mx-auto">
+                    <div class="post-heading-not">
+                        <h1><strong>La página que busca no existe</strong></h1>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</header>
+    </header>
+</template>
 
 <script>
 export default {
@@ -19,5 +21,13 @@ export default {
 </script>
 
 <style>
-
+.masthead-not {
+    margin-top: 100px;
+    color: white;
+    background-size: cover;
+    height: 400px;
+}
+.post-heading-not {
+    margin-top: 100px;
+}
 </style>
